33b3358b782cc729865cda5cf20594af62e70843
tnd4698 <tnd4698@gmail.com>
2020-06-10 11:26:04 +0700

    update script

--- scripts/model.sql
+++ scripts/model.sql
@@ -1,9 +1,10 @@
 CREATE TABLE "model" (
   "id" bigint PRIMARY KEY,
   "workspace_id" bigint NOT NULL,
-  "name" varchar(128) NOT NULL DEFAULT ,
-  "type" varchar(64) NOT NULL DEFAULT 'Business',
+  "name" varchar(128) NOT NULL DEFAULT 'Model Name',
+  "type" int NOT NULL,
   "time_frame" varchar(64) NOT NULL DEFAULT '',
+  "buz_type" varchar(64) NOT NULL DEFAULT 'Business',
   "description" text NOT NULL DEFAULT '',
   "files" varchar(512) NOT NULL DEFAULT ''
 );
--- scripts/position.sql
+++ scripts/position.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "position" (
   "id" bigint PRIMARY KEY,
   "workspace_id" bigint NOT NULL,
-  "name" varchar(128) NOT NULL DEFAULT ,
-  "type" varchar(64) NOT NULL DEFAULT 'Business',
+  "name" varchar(128) NOT NULL DEFAULT "Position Name",
+  "buz_type" varchar(64) NOT NULL DEFAULT 'Business',
   "time_frame" varchar(64) NOT NULL DEFAULT '',
   "description" text NOT NULL DEFAULT '',
   "files" varchar(512) NOT NULL DEFAULT ''
