98471f9ba47e1556ee361a717022fafd926744db
devlogex <tannd.devlogex@gmail.com>
2021-01-26 13:17:52 +0000

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM pw_env
 WORKDIR /data/dbservice
-COPY . .
-RUN mvn -B -f /data/dbservice/pom.xml -s /usr/share/maven/ref/settings.xml clean package
-CMD ["java", "-jar", "runner/target/dbservice-runner-jar-with-dependencies.jar"]
+COPY runner/target/dbservice-runner-jar-with-dependencies.jar .
+# RUN mvn -B -f /data/dbservice/pom.xml -s /usr/share/maven/ref/settings.xml clean package
+CMD ["java", "-jar", "dbservice-runner-jar-with-dependencies.jar"]
--- scripts/index.sql
+++ scripts/index.sql
@@ -8,23 +8,6 @@ CREATE TABLE "package_code" (
   "updated_at" bigint,
   "updated_by" bigint,
   "state" int NOT NULL
-);CREATE TABLE "package" (
-  "id" bigint PRIMARY KEY,
-  "max_product" int NOT NULL,
-  "max_member" int NOT NULL,
-  "price" NUMERIC(19,4) NOT NULL,
-  "period_validity" bigint NOT NULL,
-  "name" VARCHAR(256),
-  "description" text,
-  "created_at" bigint,
-  "created_by" bigint,
-  "updated_at" bigint,
-  "updated_by" bigint,
-  "state" int NOT NULL
-);CREATE TABLE "permission" (
-  "id" bigint PRIMARY KEY,
-  "name" bigint UNIQUE NOT NULL,
-  "permissions" text NOT NULL
 );CREATE TABLE "product" (
   "id" bigint PRIMARY KEY,
   "name" varchar(256) NOT NULL,
@@ -62,22 +45,35 @@ CREATE TABLE "package_code" (
   "created_by" bigint,
   "updated_at" bigint,
   "updated_by" bigint
-);CREATE TABLE "workspace_config" (
-  "id" bigint PRIMARY KEY,
-  "workspace_id" bigint,
-  "package_id" bigint,
-  "config" text NOT NULL,
-  "state" int NOT NULL,
-  "created_at" bigint,
-  "created_by" bigint,
-  "updated_at" bigint,
-  "updated_by" bigint
+);
+
+INSERT INTO user_profile(id, email, password, role, first_name, last_name, company_name, domain, state)
+VALUES (
+  1,
+  'admin@gmail.com',
+  'admin',
+  'SYSTEM_ADMIN',
+  'admin',
+  'admin',
+  'admin',
+  'admin',
+  0
 );CREATE TABLE "workspace" (
   "id" bigint PRIMARY KEY,
   "state" int NOT NULL,
   "config_id" bigint,
   "owner_id" bigint NOT NULL,
   "domain" varchar(256) NOT NULL UNIQUE,
+  "created_at" bigint,
+  "created_by" bigint,
+  "updated_at" bigint,
+  "updated_by" bigint
+);CREATE TABLE "workspace_config" (
+  "id" bigint PRIMARY KEY,
+  "workspace_id" bigint,
+  "package_id" bigint,
+  "config" text NOT NULL,
+  "state" int NOT NULL,
   "created_at" bigint,
   "created_by" bigint,
   "updated_at" bigint,
@@ -129,21 +125,6 @@ CREATE TABLE "epic" (
   "is_complete" integer default 0,
   "created_at" bigint,
   "created_by" bigint
-);CREATE TABLE "release_layout" (
-  "id" bigint PRIMARY KEY,
-  "release_id" bigint,
-  "product_id" bigint,
-  "type" varchar(64),
-  "layout" text
-);CREATE TABLE "release_phase" (
-  "id" bigint PRIMARY KEY,
-  "release_id" bigint NOT NULL,
-  "name" varchar(256) NOT NULL,
-  "type" integer NOT NULL,
-  "color" varchar(8),
-  "date" varchar(64),
-  "description" text,
-  "files" text
 );CREATE TABLE "release" (
   "id" bigint PRIMARY KEY,
   "product_id" bigint NOT NULL,
@@ -165,6 +146,21 @@ CREATE TABLE "epic" (
   "type" varchar(64),
   "created_at" bigint,
   "created_by" bigint
+);CREATE TABLE "release_layout" (
+  "id" bigint PRIMARY KEY,
+  "release_id" bigint,
+  "product_id" bigint,
+  "type" varchar(64),
+  "layout" text
+);CREATE TABLE "release_phase" (
+  "id" bigint PRIMARY KEY,
+  "release_id" bigint NOT NULL,
+  "name" varchar(256) NOT NULL,
+  "type" integer NOT NULL,
+  "color" varchar(8),
+  "date" varchar(64),
+  "description" text,
+  "files" text
 );CREATE TABLE "requirement" (
   "id" bigint PRIMARY KEY,
   "feature_id" bigint NOT NULL,
@@ -180,6 +176,7 @@ CREATE TABLE "epic" (
   "product_id" bigint NOT NULL,
   "name" varchar(256) NOT NULL,
   "state" integer NOT NULL,
+  "personas" text DEFAULT '[]',
   "steps" text,
   "epics" text,
   "releases" text,
@@ -214,15 +211,15 @@ CREATE TABLE "goal"
 (
     "id" bigint PRIMARY KEY,
     "product_id" bigint NOT NULL,
-    "name" varchar(512) NOT NULL DEFAULT 'Goal...',
-    "description" text NOT NULL DEFAULT '',
-    "files" varchar(512) NOT NULL DEFAULT '',
+    "name" varchar(512) DEFAULT 'Goal...',
+    "description" text DEFAULT '',
+    "files" text,
     "parent_goal" text,
     "initiatives" text default '[]',
     "process" INTEGER default 0,
     "status" integer,
-    "time_frame" varchar(64) NOT NULL DEFAULT '',
-    "color" varchar(8) NOT NULL DEFAULT '#5DBF40',
+    "time_frame" varchar(64) DEFAULT '',
+    "color" varchar(8) DEFAULT '#5DBF40',
     "metric" text,
     "metric_description" text,
     "metric_file" text
@@ -231,15 +228,15 @@ CREATE TABLE "initiative"
 (
     "id" bigint PRIMARY KEY,
     "product_id" bigint NOT NULL,
-    "name" varchar(512) NOT NULL DEFAULT 'Goal...',
-    "description" text NOT NULL DEFAULT '',
-    "files" varchar(512) NOT NULL DEFAULT '',
+    "name" varchar(512) DEFAULT 'Goal...',
+    "description" text DEFAULT '',
+    "files" text DEFAULT '',
     "parent_initiative" text,
     "goals" text default '[]',
     "process" INTEGER default 0,
     "status" integer,
-    "time_frame" varchar(64) NOT NULL DEFAULT '',
-    "color" varchar(8) NOT NULL DEFAULT '#5DBF40',
+    "time_frame" varchar(64) DEFAULT '',
+    "color" varchar(8) DEFAULT '#5DBF40',
     "start_at" bigint,
     "end_at" bigint,
     "visible" int DEFAULT 1
@@ -250,64 +247,64 @@ CREATE TABLE "layout" (
   "type" varchar(64) NOT NULL,
   "layout" text NOT NULL DEFAULT ''
 );
+CREATE TABLE "model" (
+  "id" bigint PRIMARY KEY,
+  "product_id" bigint NOT NULL,
+  "name" varchar(128) DEFAULT 'Model Name',
+  "type" int NOT NULL,
+  "time_frame" varchar(64) DEFAULT '',
+  "buz_type" varchar(64) DEFAULT 'Business',
+  "description" text DEFAULT '',
+  "files" text DEFAULT ''
+);
 CREATE TABLE "model_component" (
   "id" bigint PRIMARY KEY,
   "name" varchar(64) NOT NULL DEFAULT 'Name...',
   "model_id" bigint NOT NULL,
   "color" varchar(8) NOT NULL DEFAULT 'f5f6fa',
-  "description" text NOT NULL DEFAULT 'Description...',
-  "files" text NOT NULL DEFAULT ''
-);
-CREATE TABLE "model" (
-  "id" bigint PRIMARY KEY,
-  "product_id" bigint NOT NULL,
-  "name" varchar(128) NOT NULL DEFAULT 'Model Name',
-  "type" int NOT NULL,
-  "time_frame" varchar(64) NOT NULL DEFAULT '',
-  "buz_type" varchar(64) NOT NULL DEFAULT 'Business',
-  "description" text NOT NULL DEFAULT '',
-  "files" text NOT NULL DEFAULT ''
+  "description" text DEFAULT 'Description...',
+  "files" text DEFAULT ''
 );
 CREATE TABLE "personas"
 (
     "id" bigint PRIMARY KEY,
     "product_id" bigint NOT NULL,
-    "name" varchar(64) NOT NULL DEFAULT 'Name...',
+    "name" varchar(64) DEFAULT 'Name...',
     "image" text,
-    "color" varchar(8) NOT NULL DEFAULT 'f5f6fa',
+    "color" varchar(8) DEFAULT 'f5f6fa',
     "content" text
-);
-CREATE TABLE "position_component" (
-  "id" bigint PRIMARY KEY,
-  "name" varchar(64) NOT NULL DEFAULT 'Name...',
-  "position_id" bigint NOT NULL,
-  "color" varchar(8) NOT NULL DEFAULT 'f5f6fa',
-  "description" text NOT NULL DEFAULT 'Description...',
-  "files" text NOT NULL DEFAULT ''
 );
 CREATE TABLE "position" (
   "id" bigint PRIMARY KEY,
   "product_id" bigint NOT NULL,
-  "name" varchar(128) NOT NULL DEFAULT 'Position Name',
-  "buz_type" varchar(64) NOT NULL DEFAULT 'Business',
-  "time_frame" varchar(64) NOT NULL DEFAULT '',
-  "description" text NOT NULL DEFAULT '',
-  "files" text NOT NULL DEFAULT ''
+  "name" varchar(128) DEFAULT 'Position Name',
+  "buz_type" varchar(64) DEFAULT 'Business',
+  "time_frame" varchar(64) DEFAULT '',
+  "description" text DEFAULT '',
+  "files" text DEFAULT ''
 );
-CREATE TABLE "vision_component" (
+CREATE TABLE "position_component" (
   "id" bigint PRIMARY KEY,
-  "name" varchar(64) NOT NULL DEFAULT 'Strategy...',
-  "vision_id" bigint NOT NULL,
-  "summary" text NOT NULL DEFAULT 'Summary...',
-  "color" varchar(8) NOT NULL DEFAULT 'f5f6fa',
-  "description" text NOT NULL DEFAULT 'Description...',
-  "files" text NOT NULL DEFAULT ''
+  "name" varchar(64) DEFAULT 'Name...',
+  "position_id" bigint NOT NULL,
+  "color" varchar(8) DEFAULT 'f5f6fa',
+  "description" text DEFAULT 'Description...',
+  "files" text DEFAULT ''
 );
 CREATE TABLE "vision" (
   "id" bigint PRIMARY KEY,
   "product_id" bigint NOT NULL UNIQUE,
-  "description" text NOT NULL DEFAULT '',
-  "files" text NOT NULL DEFAULT ''
+  "description" text DEFAULT '',
+  "files" text DEFAULT ''
+);
+CREATE TABLE "vision_component" (
+  "id" bigint PRIMARY KEY,
+  "name" varchar(64) DEFAULT 'Strategy...',
+  "vision_id" bigint NOT NULL,
+  "summary" text DEFAULT 'Summary...',
+  "color" varchar(8) DEFAULT 'f5f6fa',
+  "description" text DEFAULT 'Description...',
+  "files" text DEFAULT ''
 );
 CREATE TABLE "comment" (
   "id" bigint PRIMARY KEY,
@@ -317,14 +314,6 @@ CREATE TABLE "comment" (
   "files" text,
   "created_at" bigint,
   "created_by" bigint
-);CREATE TABLE "todo_assign" (
-  "id" bigint PRIMARY KEY,
-  "todo_id" bigint NOT NULL,
-  "user_id" bigint NOT NULL,
-  "workspace_id" bigint NOT NULL,
-  "type" integer NOT NULL,
-  "state" integer NOT NULL,
-  "verified_at" bigint
 );CREATE TABLE "todo" (
   "id" bigint PRIMARY KEY,
   "belong_id" bigint NOT NULL,
@@ -338,4 +327,12 @@ CREATE TABLE "comment" (
   "completed_at" bigint,
   "created_at" bigint NOT NULL,
   "created_by" bigint NOT NULL
+);CREATE TABLE "todo_assign" (
+  "id" bigint PRIMARY KEY,
+  "todo_id" bigint NOT NULL,
+  "user_id" bigint NOT NULL,
+  "workspace_id" bigint NOT NULL,
+  "type" integer NOT NULL,
+  "state" integer NOT NULL,
+  "verified_at" bigint
 );
